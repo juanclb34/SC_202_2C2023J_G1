d0077f69dfc0155ed192567cd37f465931143af3
Juanclb19@hotmail.com <jlacayo43235@ufide.ac.cr>
2023-07-21 10:34:11 -0600

    Inicialización del Proyecto

---
+++ src/main/java/com/mycompany/sc_202_2c2023j_g1/SC_202_2C2023J_G1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sc_202_2c2023j_g1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class SC_202_2C2023J_G1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+           System.out.println("Estoy probando");
+    }
+}
